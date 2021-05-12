06af7b1ddc28f2367f6c29e6abaa91a4efa79d12
harshitagupta11 <guptaharshita99@gmail.com>
2021-05-12 15:30:33 +0530

    basic done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useEffect, useState } from 'react';
+import './App.css';
+import MovieList from './components/MovieList';
+
+import {FEATURE_API,SEARCH_API} from './key'
+
+
+ function App() {
+
+  const [movies,setMovies]=useState([])
+  const [search,setSearch]=useState("")
+  const fetchMovies= async (url)=>{
+    let response= await fetch(url)
+    response=await response.json()
+    return response;
+   }
+
+  useEffect(()=>{
+ fetchMovies(FEATURE_API).then(data=>{
+   setMovies(data.results)
+  })
+  },[])
+
+const handleSearch=(e)=>{
+e.preventDefault();
+if(search){
+  fetchMovies(SEARCH_API+search).then(data=>{
+    setMovies(data.results)
+    
+   })
+  setSearch("")
+}
+
+}
+const handleOnChange=(e)=>{
+  console.log(e.target.value)
+  setSearch(e.target.value)
+}
+
+// console.log(movies.length)
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header >
+        <form onSubmit={handleSearch}>
+        <input  className="search"
+        type="text"
+        placeholder="Search..." 
+        value={search}
+        onChange={handleOnChange}
+        ></input>
+      </form>
       </header>
+     <MovieList movies={movies} />
     </div>
   );
 }
